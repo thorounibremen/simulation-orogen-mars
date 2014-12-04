0047a7afc4f3821b6472d4191df7dde79efb779b
Janosch Machowinski <janosch.machowinski@dfki.de>
2014-12-04 10:38:50 +0100

    fixed wrong time of MultiLevelLaserScans
    Inside the simulator base::time::now is used for
    setting the time of the scan. This is wrong, as
    the simulation time differs from real time.
    This had a great impact as the stream aggregator
    started dropping samples in every tasks that was
    processing the MLLSs.

--- tasks/MarsRotatingLaserRangeFinder.cpp
+++ tasks/MarsRotatingLaserRangeFinder.cpp
@@ -80,6 +80,7 @@ void MarsRotatingLaserRangeFinder::updateHook()
     
     if(_laser_scans.connected() && mSensor->getMultiLevelLaserScan(mScan)) {
         std::cout << "Write mlls" << std::endl;
+        mScan.time = getTime();
         _laser_scans.write(mScan);
     }
 }
